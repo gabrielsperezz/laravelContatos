d273667f94e15c9e55122abe67ea9d8be6db773e
Gabriel <gabriel.perez@fulltime.com.br>
2018-12-01 08:28:00 -0200

    Adicionado autenticação para rotas de contatos

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -15,7 +15,7 @@ class ContatoController extends Controller
 
     public function __construct()
     {
-
+        $this->middleware('auth');
     }
 
     public function listarContatos(){
--- app/Http/Controllers/ContatoTelefoneController.php
+++ app/Http/Controllers/ContatoTelefoneController.php
@@ -10,7 +10,7 @@ class ContatoTelefoneController extends Controller
 
     public function __construct()
     {
-
+        $this->middleware('auth');
     }
 
     public function buscarTelefonesPorIdContato($idContato)
